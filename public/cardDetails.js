9cf96de7ba707c2e8f37722a6239aa13c3fc612a
FranciscoDiego64 <112688191+FranciscoDiego64@users.noreply.github.com>
2023-07-03 15:17:42 +0200

    styling edits

--- public/cardDetails.js
+++ public/cardDetails.js
@@ -19,7 +19,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 cvc: `test_${cvc}`,
                 holderName: holderName
                 
-                
                /*
                 cardNumber: cardNumber,
                 expiryMonth: expiryMonth,
